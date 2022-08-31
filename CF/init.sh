cc0944c30172cc4826d1a0c91219cfa063d3140a
CoroboBred <kingcorobobred@gmail.com>
2022-08-31 15:08:12 -0400

    Add CF init

---
+++ CF/init.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+mkdir $1 $1/A $1/B $1/C $1/D $1/E $1/F  
+cp Main.java $1/A/
+cp Main.java $1/B/
+cp Main.java $1/C/
+cp Main.java $1/D/
+cp Main.java $1/E/
+cp Main.java $1/F/
+
+
+touch $1/A/in
+touch $1/A/ans
+touch $1/B/in
+touch $1/B/ans
+touch $1/C/in
+touch $1/C/ans
+touch $1/D/in
+touch $1/D/ans
+touch $1/E/in
+touch $1/E/ans
+touch $1/F/in
+touch $1/F/ans
